98e58bc25c40c0958953fbb8e373732c846874cb
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-07-13 10:44:25 +0200

    new configs

---
+++ backend/script/DeployPoolManager.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Script.sol";
+import { PoolManager } from "v4-core/PoolManager.sol";
+
+contract DeployContracts is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        PoolManager poolManager = new PoolManager(500000);
+
+        console.log("Pool Manager deployed at:", address(poolManager));
+
+        vm.stopBroadcast();
+    }
+}
