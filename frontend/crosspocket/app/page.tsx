59167e9fdfbbb11da3900d3c30b0ac3116fd3bc4
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-07-13 04:04:40 +0300

    functions added

--- frontend/crosspocket/app/page.tsx
+++ frontend/crosspocket/app/page.tsx
@@ -1,7 +1,410 @@
-import Image from "next/image";
+// pages/index.tsx veya page.tsx
+
+"use client";
+import { useEffect, useState, useCallback } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { W3SSdk } from "@circle-fin/w3s-pw-web-sdk";
+import {
+  getAppId,
+  createNewUser,
+  createSessionToken,
+  initializeAccount,
+  executeChallenge,
+  fetchWalletData,
+  fundWallet,
+  getWalletBalances,
+  initiateTransfer,
+  confirmTransaction,
+} from "./utils/functions";
 
 export default function Home() {
+  const [appId, setAppId] = useState("");
+  const [userId, setUserId] = useState("");
+  const [responseUserId, setResponseUserId] = useState(null);
+  const [tokenResponse, setTokenResponse] = useState(null);
+  const [idempotencyKey, setIdempotencyKey] = useState("");
+  const [blockchain, setBlockchain] = useState("");
+  const [accountResponse, setAccountResponse] = useState(null);
+  const [userToken, setUserToken] = useState("");
+  const [challengeId, setChallengeId] = useState("");
+  const [encryptionKey, setEncryptionKey] = useState("");
+  const [walletData, setWalletData] = useState(null);
+  const [error, setError] = useState("");
+  const [address, setAddress] = useState("");
+  const [fundResponse, setFundResponse] = useState(null);
+  const [walletId, setWalletId] = useState("");
+  const [walletBalances, setWalletBalances] = useState(null);
+  const [appIdnew, setAppIdnew] = useState("");
+  const [destinationAddress, setDestinationAddress] = useState("");
+  const [refId, setRefId] = useState("");
+  const [amounts, setAmounts] = useState([""]);
+  const [tokenId, setTokenId] = useState("");
+  const [transferResponse, setTransferResponse] = useState(null);
+  const [confirmResponse, setConfirmResponse] = useState(null);
+
+  let sdk: W3SSdk;
+
+  useEffect(() => {
+    if (appIdnew) {
+      console.log("App ID: " + appIdnew);
+    }
+  }, [appIdnew]);
+
+  useEffect(() => {
+    if (responseUserId) {
+      console.log("User ID: " + userId);
+      console.log("Response User ID: ", responseUserId);
+    }
+  }, [responseUserId]);
+
+  useEffect(() => {
+    if (tokenResponse) {
+      console.log("Session token response: ", tokenResponse);
+    }
+  }, [tokenResponse]);
+
+  useEffect(() => {
+    if (accountResponse) {
+      console.log("Account response: ", accountResponse);
+    }
+  }, [accountResponse]);
+
+  useEffect(() => {
+    if (walletData) {
+      console.log("Wallet data: ", walletData);
+    }
+  }, [walletData]);
+
+  useEffect(() => {
+    if (fundResponse) {
+      console.log("Fund response: ", fundResponse);
+    }
+  }, [fundResponse]);
+
+  useEffect(() => {
+    if (walletBalances) {
+      console.log("Wallet balances: ", walletBalances);
+    }
+  }, [walletBalances]);
+
+  useEffect(() => {
+    if (transferResponse) {
+      console.log("Transfer response: ", transferResponse);
+    }
+  }, [transferResponse]);
+
+  useEffect(() => {
+    if (confirmResponse) {
+      console.log("Confirm response: ", confirmResponse);
+    }
+  }, [confirmResponse]);
+
   return (
-    <></>
+    <div className="bg-red-300 min-h-screen flex flex-col space-y-12 justify-center items-center">
+      <div className="flex justify-center items-center">
+        <button
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+          onClick={() => getAppId(setAppIdnew)}
+        >
+          Get App Id
+        </button>
+      </div>
+      <form onSubmit={(e) => createNewUser(e, userId, setResponseUserId)} className="justify-center items-center">
+        <input
+          type="text"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+          placeholder="Enter User ID (must more than 5 characters)"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <button
+          type="submit"
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+        >
+          Create User
+        </button>
+      </form>
+      <form onSubmit={(e) => createSessionToken(e, userId, setTokenResponse)}>
+        <input
+          type="text"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+          placeholder="Enter User ID"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <button
+          type="submit"
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+        >
+          Session Token
+        </button>
+      </form>
+      <form onSubmit={(e) => initializeAccount(e, userToken, idempotencyKey, blockchain, setAccountResponse)}>
+        <input
+          type="text"
+          value={userToken}
+          onChange={(e) => setUserToken(e.target.value)}
+          placeholder="Enter User Token"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <input
+          type="text"
+          value={idempotencyKey}
+          onChange={(e) => setIdempotencyKey(e.target.value)}
+          placeholder="Enter Idempotency Key"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <input
+          type="text"
+          value={blockchain}
+          onChange={(e) => setBlockchain(e.target.value)}
+          placeholder="Enter Blockchain"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <button
+          type="submit"
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+        >
+          Initialize Account
+        </button>
+      </form>
+      <form onSubmit={(e) => executeChallenge(e, sdk, appId, userToken, encryptionKey, challengeId, toast)}>
+        <input
+          type="text"
+          value={appId}
+          onChange={(e) => setAppId(e.target.value)}
+          placeholder="Enter App ID"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <input
+          type="text"
+          value={userToken}
+          onChange={(e) => setUserToken(e.target.value)}
+          placeholder="Enter User Token"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <input
+          type="text"
+          value={challengeId}
+          onChange={(e) => setChallengeId(e.target.value)}
+          placeholder="Enter Challenge ID"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <input
+          type="text"
+          value={encryptionKey}
+          onChange={(e) => setEncryptionKey(e.target.value)}
+          placeholder="Enter Encryption Key"
+          required
+          className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+        />
+        <button
+          type="submit"
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+        >
+          Execute Challenge
+        </button>
+      </form>
+      <form onSubmit={(e) => fetchWalletData(e, userToken, setError, setWalletData)}>
+        <div>
+          <input
+            type="text"
+            value={userToken}
+            onChange={(e) => setUserToken(e.target.value)}
+            required
+            placeholder="Enter User Token"
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+        >
+          Fetch Wallet Data
+        </button>
+      </form>
+      <form onSubmit={(e) => fundWallet(e, address, setError, setFundResponse)}>
+        <div>
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+            placeholder="Enter Wallet Address"
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+          />
+        </div>
+        <button
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+          type="submit"
+        >
+          Fund Wallet
+        </button>
+      </form>
+      <form onSubmit={(e) => getWalletBalances(e, walletId, setError, setWalletBalances)}>
+        <div>
+          <input
+            type="text"
+            value={walletId}
+            onChange={(e) => setWalletId(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Wallet ID"
+          />
+        </div>
+        <button
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+          type="submit"
+        >
+          Get Balances
+        </button>
+      </form>
+      <form onSubmit={(e) => initiateTransfer(e, idempotencyKey, userId, destinationAddress, refId, amounts, tokenId, walletId, userToken, setError, setTransferResponse)}>
+        <div>
+          <input
+            type="text"
+            value={idempotencyKey}
+            onChange={(e) => setIdempotencyKey(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Idempotency Key"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter User ID"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={destinationAddress}
+            onChange={(e) => setDestinationAddress(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Destination Address"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={refId}
+            onChange={(e) => setRefId(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Reference ID"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={amounts}
+            onChange={(e) => setAmounts([e.target.value])}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Amount"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={tokenId}
+            onChange={(e) => setTokenId(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Token ID"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={walletId}
+            onChange={(e) => setWalletId(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Wallet ID"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={userToken}
+            onChange={(e) => setUserToken(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter User Token"
+          />
+        </div>
+        <button
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+          type="submit"
+        >
+          Initiate Transfer
+        </button>
+      </form>
+      <form onSubmit={(e) => confirmTransaction(e, sdk, appId, userToken, encryptionKey, challengeId, toast, setConfirmResponse)}>
+        <div>
+          <input
+            type="text"
+            value={appId}
+            onChange={(e) => setAppId(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter App ID"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={userToken}
+            onChange={(e) => setUserToken(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter User Token"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={encryptionKey}
+            onChange={(e) => setEncryptionKey(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Encryption Key"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            value={challengeId}
+            onChange={(e) => setChallengeId(e.target.value)}
+            required
+            className="w-64 h-16 opacity-80 rounded-xl flex justify-center items-center"
+            placeholder="Enter Challenge ID"
+          />
+        </div>
+        <button
+          className="w-64 h-16 bg-teal-400 rounded-xl flex justify-center items-center"
+          type="submit"
+        >
+          Confirm Transaction
+        </button>
+      </form>
+      <ToastContainer />
+    </div>
   );
 }
---
+++ frontend/crosspocket/app/utils/functions.ts
@@ -0,0 +1,237 @@
+// utils/functions.ts
+import { W3SSdk } from "@circle-fin/w3s-pw-web-sdk";
+
+export const getAppId = async (setAppIdnew) => {
+  try {
+    const response = await fetch("/api/appId");
+    const result = await response.json();
+    if (result && result.data && result.data.appId) {
+      setAppIdnew(result.data.appId);
+      console.log("AppId: " + result.data.appId);
+    } else {
+      console.error("appId not found in data");
+    }
+  } catch (error) {
+    console.error("Error fetching appId:", error);
+  }
+};
+
+export const createNewUser = async (e, userId, setResponseUserId) => {
+  e.preventDefault();
+  try {
+    const res = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId }),
+    });
+    const result = await res.json();
+    setResponseUserId(result);
+  } catch (error) {
+    console.error(error);
+    setResponseUserId({ error: "An error occurred" });
+  }
+};
+
+export const createSessionToken = async (e, userId, setTokenResponse) => {
+  e.preventDefault();
+  try {
+    const res = await fetch("/api/session-token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId }),
+    });
+    const result = await res.json();
+    setTokenResponse(result);
+  } catch (error) {
+    console.error(error);
+    setTokenResponse({ error: "An error occurred" });
+  }
+};
+
+export const initializeAccount = async (e, userToken, idempotencyKey, blockchain, setAccountResponse) => {
+  e.preventDefault();
+  try {
+    const res = await fetch("/api/users/initialize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userToken,
+        idempotencyKey,
+        blockchains: [blockchain],
+      }),
+    });
+    const result = await res.json();
+    setAccountResponse(result);
+  } catch (error) {
+    console.error(error);
+    setAccountResponse({ error: "An error occurred" });
+  }
+};
+
+export const executeChallenge = async (e, sdk, appId, userToken, encryptionKey, challengeId, toast) => {
+  e.preventDefault();
+  try {
+    if (!sdk) {
+      sdk = new W3SSdk();
+    }
+
+    sdk.setAppSettings({ appId });
+    sdk.setAuthentication({ userToken, encryptionKey });
+
+    sdk.execute(challengeId, (error, result) => {
+      if (error) {
+        toast.error(`Error: ${error?.message ?? "Error!"}`);
+        return;
+      }
+      toast.success(`Challenge: ${result?.type}, Status: ${result?.status}`);
+      console.log("Challenge executed: ", result);
+    });
+  } catch (error) {
+    console.error("Error executing challenge:", error.message);
+    toast.error("An error occurred while executing the challenge.");
+  }
+};
+
+export const fetchWalletData = async (e, userToken, setError, setWalletData) => {
+  e.preventDefault();
+  setError("");
+  setWalletData(null);
+
+  try {
+    const response = await fetch("/api/checkWalletStatus", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userToken }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred");
+    }
+    setWalletData(data);
+  } catch (error) {
+    console.error(error);
+    setError(error.message);
+  }
+};
+
+export const fundWallet = async (e, address, setError, setFundResponse) => {
+  e.preventDefault();
+  setError("");
+  setFundResponse(null);
+
+  try {
+    const response = await fetch("/api/fundWallet", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ address }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred");
+    }
+    setFundResponse(data);
+  } catch (error) {
+    console.error(error);
+    setError(error.message);
+  }
+};
+
+export const getWalletBalances = async (e, walletId, setError, setWalletBalances) => {
+  e.preventDefault();
+  setError("");
+  setWalletBalances(null);
+
+  try {
+    const response = await fetch("/api/getWalletBalance", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ walletId }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred");
+    }
+    setWalletBalances(data);
+  } catch (error) {
+    console.error(error);
+    setError(error.message);
+  }
+};
+
+export const initiateTransfer = async (e, idempotencyKey, userId, destinationAddress, refId, amounts, tokenId, walletId, userToken, setError, setTransferResponse) => {
+  e.preventDefault();
+  setError("");
+  setTransferResponse(null);
+
+  try {
+    const response = await fetch("/api/transfer", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        idempotencyKey,
+        userId,
+        destinationAddress,
+        refId,
+        amounts,
+        tokenId,
+        walletId,
+        userToken,
+      }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.error || "An error occurred");
+    }
+    setTransferResponse(data);
+  } catch (error) {
+    console.error(error);
+    setError(error.message);
+  }
+};
+
+export const confirmTransaction = async (e, sdk, appId, userToken, encryptionKey, challengeId, toast, setConfirmResponse) => {
+  e.preventDefault();
+  try {
+    if (!sdk) {
+      sdk = new W3SSdk();
+    }
+
+    sdk.setAppSettings({ appId });
+
+    sdk.setAuthentication({
+      userToken,
+      encryptionKey,
+    });
+
+    sdk.execute(challengeId, (error, result) => {
+      if (error) {
+        toast.error(`Error: ${error?.message ?? "Error!"}`);
+        return;
+      }
+      toast.success(`Transaction confirmed: ${result?.type}, Status: ${result?.status}`);
+      console.log("Transaction confirmed: ", result);
+      setConfirmResponse(result);
+    });
+  } catch (error) {
+    console.error("Error confirming transaction:", error.message);
+    toast.error("An error occurred while confirming the transaction.");
+  }
+};
