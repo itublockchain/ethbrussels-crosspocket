7409da88d1a80066d4d9ba68ebfd2ddcccc5a800
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-07-14 05:52:55 +0300

    fix

--- frontend/crosspocket/app/components/header.tsx
+++ frontend/crosspocket/app/components/header.tsx
@@ -7,9 +7,9 @@ import {
   initializeAccount,
   executeChallenge,
   fetchWalletData,
-} from "../utils/functions"; 
+} from "../utils/functions";
 import { W3SSdk } from "@circle-fin/w3s-pw-web-sdk";
-import { toast,ToastContainer } from "react-toastify"; 
+import { toast, ToastContainer } from "react-toastify";
 
 const Header = () => {
   const handleCreateAccount = async (e) => {
@@ -19,20 +19,21 @@ const Header = () => {
       await createSessionToken();
       await initializeAccount();
       const sdk = new W3SSdk();
-      const appId = "d6a14aa4-0e4e-5778-aff7-f34d51486a53"; 
+      const appId = "d6a14aa4-0e4e-5778-aff7-f34d51486a53";
       await executeChallenge(e, sdk, appId, toast);
-  
-      
+
       setTimeout(async () => {
         try {
           await fetchWalletData();
           toast.success("Account created and wallet data fetched successfully");
+          window.location.reload();
         } catch (error) {
-          toast.error(`An error occurred while fetching wallet data: ${error.message}`);
+          toast.error(
+            `An error occurred while fetching wallet data: ${error.message}`
+          );
           console.error("Error in fetching wallet data:", error);
         }
       }, 20000);
-  
     } catch (error) {
       toast.error(`An error occurred: ${error.message}`);
       console.error("Error in account creation process:", error);
@@ -58,16 +59,28 @@ const Header = () => {
           width={200}
           height={200}
           className="pt-4"
-        
         />
         <div className="bg-[#18227b] w-[580px] h-16 justify-center flex items-center rounded-b-full flex-row space-x-10">
-          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-20 flex text-center justify-center items-center rounded-2xl">Home</h1>
-          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-20 flex text-center justify-center items-center rounded-2xl">About</h1>
-          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-24 flex text-center justify-center items-center rounded-2xl">Contact</h1>
-          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-16 flex text-center justify-center items-center rounded-2xl">Blog</h1>
+          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-20 flex text-center justify-center items-center rounded-2xl">
+            Home
+          </h1>
+          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-20 flex text-center justify-center items-center rounded-2xl">
+            About
+          </h1>
+          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-24 flex text-center justify-center items-center rounded-2xl">
+            Contact
+          </h1>
+          <h1 className="text-[#C4CAFF] text-lg bg-[#2E3993] h-10 w-16 flex text-center justify-center items-center rounded-2xl">
+            Blog
+          </h1>
         </div>
         <div className="flex pt-4">
-            <button className="bg-[#4757E4] h-14 w-64 text-2xl text-[#C4CAFF] rounded-2xl" onClick={handleCreateAccount}>{walletAddress ? formatAddress(walletAddress) : "Create Account"}</button>
+          <button
+            className="bg-[#4757E4] h-14 w-64 text-2xl text-[#C4CAFF] rounded-2xl"
+            onClick={handleCreateAccount}
+          >
+            {walletAddress ? formatAddress(walletAddress) : "Create Account"}
+          </button>
         </div>
       </nav>
       <ToastContainer />
